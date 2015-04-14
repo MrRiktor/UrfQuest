c00252789f3934482472e2fbddee53a71115cd81
SabinXna <Sabin-@live.com>
2015-04-13 23:22:15 -0400

    ProgressionView completed
    Added dynamic background images
    Added dynamic enemy portraits

--- RiotAPIChallenge/Assets/Source/GameStates/ProgressionState/ProgressionUI/MainUI/ProgressionView.cs
+++ RiotAPIChallenge/Assets/Source/GameStates/ProgressionState/ProgressionUI/MainUI/ProgressionView.cs
@@ -1,16 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System;
 
 public class ProgressionView : MonoBehaviour
 {
     [SerializeField]
     private Image mainImage;
 
+    [SerializeField]
+    private GameObject EnemyPortraiItem;
+
+    [SerializeField]
+    private GridLayoutGroup grid;
+
     void Start( )
     {
-        Debug.Log( GameData.StageMap.Stages [ GameData.CurrentLevel ].StageImage );
-        mainImage.sprite = Resources.Load<Sprite>(GameData.StageMap.Stages [ GameData.CurrentLevel ].StageImage);
+        Stage curStage = GameData.StageMap.Stages [ GameData.CurrentLevel ];
+
+        mainImage.sprite = Resources.Load<Sprite>( curStage.StageImage );
+
+        for ( Int32 i = 0; i < curStage.Enemies.Length; i++ )
+        {
+            Enemy curEnemy = curStage.Enemies [ i ];
+
+            GameObject item = ( GameObject )Instantiate( EnemyPortraiItem, Vector3.zero, Quaternion.identity );
+
+            if ( item == null || item.GetComponent<EnemyPortraitItem>( ) == null )
+                return;
+
+            EnemyPortraitItem itemScript = item.GetComponent<EnemyPortraitItem>( );
+            itemScript.SetEnemyPortrait( curEnemy.Portrait );
+
+            item.transform.SetParent( grid.transform );
+            item.transform.localScale = new Vector3( 1.0f, 1.0f, 1.0f );
+            item.transform.localPosition = Vector3.zero;
+        }
+
+
     }
 
     /// <summary>
---
+++ RiotAPIChallenge/Assets/Source/GameStates/ProgressionState/ProgressionUI/UIComponents/EnemyPortraitItem.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class EnemyPortraitItem : MonoBehaviour
+{
+    [SerializeField]
+    Image mainBackground;
+
+    public void SetEnemyPortrait( Sprite portrait )
+    {
+        mainBackground.sprite = portrait;
+    }
+
+}
